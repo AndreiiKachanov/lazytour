68b0eca5650f890e8edaf09dd2bd4cf9f6646dc6
Andy <andreii.kachanov@aily.team>
2024-04-02 01:01:42 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lazytour</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="Stylesheet" type="text/css" href="css/smoothDivScroll.css" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>			
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<div class="top_menu">
+					<div class="child_left">
+						Ваш город: <span class="dashed_border city">Москва</span>
+					</div>
+					<div class="child_center">
+						<ul>
+							<li><a href="">Турагентам</a></li>
+							<li><a href="">Как это работает</a></li>
+							<li><a href="">Отзывы</a></li>
+						</ul>		
+					</div>
+					<div class="child_right">
+						<ul>
+							<li class="dashed_border"><a href="">Вход</a></li>
+							<li id="separator"></li>
+							<li><a href="">Регистрация</a></li>
+						</ul>
+					</div>
+					<div class="protrusion"></div>		
+				</div>				
+				<div class="lazytour">
+					<h1 class="logo">
+						<a href="#">Lazytour</a>
+					</h1>
+					<div class="recreation">
+						<p>Отдых начинается<br>с поиска отдыха</p>
+					</div>
+					<div class="left_shadow"></div>
+					<div class="absolute"></div>
+					<button type="button" value="" class="button_claim">Оставить заявку</button>
+				</div>
+			</header>
+			<div id="page_info">	
+					<ul class="breadcrumbs">
+						<li><a href="">Главная</a>&nbsp;/&nbsp;</li>
+						<li><a href="">Все отзывы</a></li>
+					</ul>
+					<h1 class="page_name">Люди говорят</h1>
+			</div>
+			<div class="colored_line"></div>
+			<div class="shadow_line"></div>
+			<div class="container">
+				<div class="row stickem-container">
+					<div  class="content">
+								<ul>
+									<li class="container_li">
+										<div class="date_block">			
+											<div class="date">20.01.2014<span></span></div>							
+										</div>
+										<div class="author_block">
+											<img src="img/avatars/1.jpg" alt="">
+											<p class="name">Анастасия Константинопольская</p>
+											<p class="profession">Домохозяйка</p>								
+										</div>
+										<article class="comment more">
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег.
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег. 
+										</article>
+									</li>
+																		<li class="container_li">
+										<div class="date_block">			
+											<div class="date">20.01.2014<span></span></div>							
+										</div>
+										<div class="author_block">
+											<img src="img/avatars/1.jpg" alt="">
+											<p class="name">Анастасия Константинопольская</p>
+											<p class="profession">Домохозяйка</p>								
+										</div>
+										<article class="comment more">
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег.
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег. 
+										</article>
+									</li>
+																		<li class="container_li">
+										<div class="date_block">			
+											<div class="date">20.01.2014<span></span></div>							
+										</div>
+										<div class="author_block">
+											<img src="img/avatars/1.jpg" alt="">
+											<p class="name">Анастасия Константинопольская</p>
+											<p class="profession">Домохозяйка</p>								
+										</div>
+										<article class="comment more">
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег.
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег. 
+										</article>
+									</li>
+									<li class="container_li">
+										<div class="date_block">			
+											<div class="date">20.01.2014<span></span></div>							
+										</div>
+										<div class="author_block">
+											<img src="img/avatars/1.jpg" alt="">
+											<p class="name">Анастасия Константинопольская</p>
+											<p class="profession">Домохозяйка</p>								
+										</div>
+										<article class="comment more">
+												Отсюда естественно следует,
+												 что личность топ менеджера все еще интересна для многих. 
+												 Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую
+												  информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства
+												  . Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь 
+												  на опыт западных коллег.
+										</article>
+									</li>
+																	<li class="container_li">
+										<div class="date_block">			
+											<div class="date">20.01.2014<span></span></div>							
+										</div>
+										<div class="author_block">
+											<img src="img/avatars/1.jpg" alt="">
+											<p class="name">Анастасия Константинопольская</p>
+											<p class="profession">Домохозяйка</p>								
+										</div>
+										<article class="comment more">
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег.
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег. 
+										</article>
+									</li>
+																	<li class="container_li">
+										<div class="date_block">			
+											<div class="date">20.01.2014<span></span></div>							
+										</div>
+										<div class="author_block">
+											<img src="img/avatars/1.jpg" alt="">
+											<p class="name">Анастасия Константинопольская</p>
+											<p class="profession">Домохозяйка</p>								
+										</div>
+										<article class="comment more">
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег.
+												Отсюда естественно следует, что личность топ менеджера все еще интересна для многих. Тем не менее, стратегическое планирование программирует направленный маркетинг, полагаясь на инсайдерскую информацию. Продукт создает бюджет на размещение, осознав маркетинг как часть производства. Искусство медиапланирования последовательно позиционирует рекламный блок, опираясь на опыт западных коллег. 
+										</article>
+									</li>																								
+								</ul>
+								<div class="pagination">
+									<ul>
+									<li><a href="">1</a></li>
+									<li><a href="">2</a></li>
+									<li><a href="">3</a></li>
+									<li><a href="">4</a></li>
+									<li><a href="">5</a></li>
+									<li><a href="">6</a></li>
+								</ul>	
+								</div>	
+					</div>
+
+					<div class="stickem" id="feedback_form">
+
+						<form action="#" method="POST" name="form">
+
+							<h2>Оставить комментирий</h2>
+							<div class="email">
+								<label for="check1">Электронная почта</label>
+								<input type="text" name="email" id="check1" placeholder="Ваша почта">
+								<label for="check2">Имя</label>
+								<input type="text" name="name" id="check2" placeholder="Ваше имя">														
+								<div class="customfile-container">
+								<label for="check3">Фото</label>
+								<input type="file" id="file" name="myfiles[]">
+							</div>
+							</div>
+							
+							<div class="question">
+								<label for="check4">Вопрос</label>
+								<textarea name="textarea" placeholder="Ваш вопрос" id="check4"></textarea>
+								<input type="submit" name="submit" value="Отправить">
+							</div>
+						</form>
+					</div>
+				</div>	
+			</div>					
+				<div class="colored_line clear"></div>
+				<div id="leave_application">
+					<div class="footer_left">
+						<p> &copy; 2015 <a href=""> Lazytour</a></p>
+						<p class="copyright">Самый лучший способ найти тур или попутчика. А кроме этого обеспечить туристическому агенству, стабильный и огромный поток клиентов.</p>
+							<a href="">
+								<img src="img/tur_agenstvam.png" alt="">
+								<p class="tur_atenstvam">Тур. агенствам</p>
+							</a>
+					</div>
+					<div class="footer_right">
+						<p>Отдых еще не начался, а вы уже устали от поиска его места?</p>
+						<p>Оставьте заявку и тур. агентства сами будут присылать вам свои предложения.</p>
+						<button type="button"  value="" class="">Оставить заявку</button>
+					</div>
+					<div class="footer_center">
+						<ul>
+							<li><a href="">О компании</a></li>
+							<li><a href="">Как это работает</a></li>
+							<li><a href="">Отзывы</a></li>
+							<li><a href="">Контакты</a></li>
+						</ul>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div id="carousel">
+					<h1>Куда едем?</h1>
+						<div id="makeMeScrollable">
+							<a class="flag1" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Abkhazia.png" alt="Абхазия" />
+								<span>Абхазия</span>
+							</a>
+							<a class="flag2" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Afghanistan.png" alt="Абхазия" />
+								<span>Афганистан</span>
+							</a>
+							<a class="flag3" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Aland.png" alt="Абхазия" />
+								<span>Аланд</span>
+							</a>
+							<a class="flag4" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Albania.png" alt="Абхазия" />
+								<span>Албания</span>
+							</a>
+							<a class="flag5" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Algeria.png" alt="Абхазия" />
+								<span>Алжир</span>
+							</a>
+							<a class="flag6" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/American-Samoa.png" alt="Абхазия" />
+								<span>Америка_Самоа</span>
+							</a>
+							<a class="flag7" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Andorra.png" alt="Абхазия" />
+								<span>Андорра</span>
+							</a>
+							<a class="flag8" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Angola.png" alt="Абхазия" />
+								<span>Ангола</span>
+							</a>
+							<a class="flag9" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Anguilla.png" alt="Абхазия" />
+								<span>Ангуила</span>
+							</a>
+							<a class="flag10" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Antarctica.png" alt="Абхазия" />
+								<span>Антарктика</span>
+							</a>
+							<a class="flag11" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Antigua-and-Barbuda.png" alt="Абхазия" />
+								<span>Антика_и_Барбуда</span>
+							</a>
+							<a class="flag12" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Argentina.png" alt="Абхазия" />
+								<span>Аргентина</span>
+							</a>
+							<a class="flag13" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Armenia.png" alt="Абхазия" />
+								<span>Армения</span>
+							</a>
+							<a class="flag14" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Aruba.png" alt="Абхазия" />
+								<span>Аруба</span>
+							</a>
+							<a class="flag15" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Australia.png" alt="Абхазия" />
+								<span>Австралия</span>
+							</a>
+							<a class="flag16" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Austria.png" alt="Абхазия" />
+								<span>Австрия</span>
+							</a>
+							<a class="flag17" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Azerbaijan.png" alt="Абхазия" />
+								<span>Азербайджан</span>
+							</a>
+							<a class="flag18" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Bahamas.png" alt="Абхазия" />
+								<span>Багамы</span>
+							</a>
+							<a class="flag19" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Bahrain.png" alt="Абхазия" />
+								<span>Бахреин</span>
+							</a>
+							<a class="flag20" target="_blank" href="http://www.yandex.ru">
+								<img src="img/flags/Bangladesh.png" alt="Абхазия" />
+								<span>Бангладеш</span>
+							</a>																				
+						</div>
+				</div>
+				<div class="zigzaz"></div>
+				<div id="about_company">
+					<h1>Мы ищем — вы отдыхаете!</h1>
+					<p>Наша Туристическая компания Lazytour, уже более 10 лет 
+					обеспечивает для своих клиентов отдых и экскурсии на
+					 ВСЕХ островах и континентах.</p>
+					<blockquote>Как туристическое агентство, мы предлагаем пакетные туры от надежных туроператоров во все уголки мира, куда регулярно летают российские туристы. Помимо этого, как туроператор по индивидуальному туризму мы организуем любые индивидуальные и групповые туры и деловые поездки в любую точку света. </blockquote>
+					<p>Мы организовывали призовые поездки для «Сименс» в Бразилию, проводили международные конференции «MTV» в Китае и Мексике, организовывали поездку на выставку собак для Российской Кинологической Ассоциации в Бенилюкс.</p>
+					<h1>Отдых и только отдых</h1>
+					<p>Работаем по корпоративным программам обслуживания 
+					сотрудников с несколькими банками, государственными и частными компаниями. Занимаемся организацией командировок для сотрудничающих с нами организаций.</p>
+					<p class="above_ul">Выполняем Государственные контракты по приему детских групп в Москве:</p>
+					<ul>
+						<li>Китай;</li>
+						<li>Москва;</li>
+						<li>Дубаи.</li>
+					</ul>
+					<p>Организовывали поездку на выставку собак для Российской Кинологической Ассоциации в Бенилюкс.</p>
+					<h1>Море везде</h1>
+					<p>Но все равно самой любимой и хорошо знакомой нам ежедневной работой остается отправка наших друзей отдыхать. Почти все наши клиенты становятся нашими друзьями и ездят отдыхать, полагаясь на рекомендации наших менеджеров.</p>
+					<h1>Как мы выбираем туры?</h1>
+					<p>Из года в год, с нашей помощью, все больше и больше туристов приобщаются к мировой истории и географии.</p>
+					<ol>
+						<li>Неоднократно организовывали "Поезда Памяти", в которых дети общаются с ветеранами, посещают места боевой славы и Города-Герои.</li>
+						<li>Выполняем Государственные контракты по приему детских групп в Москве.</li>
+						<li>Работаем по корпоративным программам обслуживания сотрудников.</li>
+					</ol>
+					<h1>Нашли для вас лучшее</h1>
+					<p>Нужно ли вам отдохнуть семьей в Турции или Испании, посмотреть за недельку пол Европы с друзьями, покататься в Альпах на лыжах</p>
+					<h1>Мы ищем — вы отдыхаете!</h1>
+					<p>Понырять с аквалангом на Мальдивах, потанцевать на самбадроме в Рио или поехать на выходные в Париж с любимым человеком, Вы можете быть уверены, что мы Вам предложим самое лучшее, что есть на рынке.</p>
+				</div>
+			<footer>
+				<ul>
+					<li><span>&copy; 2015 </span><a href=""> Lazytour</a></li>
+					<li><a href="">О компании</a></li>
+					<li><a href="">Как это работает</a></li>
+					<li><a href="">Отзывы</a></li> 
+					<li><a href="">Контакты</a></li>
+					<li>
+						<a href="">
+							<img src="img/tur_agenstvam.png" alt=""> 
+							<span>Тур. агенствам</span>
+						</a>
+					</li>
+				</ul>
+				<div class="clear"></div>	
+			</footer>	
+		</div>
+	<script src="js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+	<script src="js/jquery.mousewheel.min.js" type="text/javascript"></script>
+	<script src="js/jquery.kinetic.min.js" type="text/javascript"></script>
+	<script src="js/jquery.smoothdivscroll-1.3-min.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		// Initialize the plugin with no custom options
+		$(document).ready(function () {
+			// None of the options are set
+			$("div#makeMeScrollable").smoothDivScroll({
+				autoScrollingMode: "onStart"
+			});
+		});
+	</script>															
+	<script type="text/javascript" src="js/select_photo.js"></script>					
+	<script type="text/javascript" src="js/hide_comments.js"></script>
+	<script type="text/javascript" src="js/jquery.stickem.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('.container').stickem();
+		});
+	</script>
+	</body>
+</html>
